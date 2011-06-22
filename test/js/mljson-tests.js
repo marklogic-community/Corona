232c9b8af868a2610e2cfff9dc15f74c99b0c474
Ryan Grimm <grimm@xqdev.com>
2011-06-22 00:35:09 -0700

    Made a few changes to the tests so they compare the JSON objects instead of the JSON strings.  Also removed the query parsing tests as most of the tested code is going away.

--- test/js/mljson-tests.js
+++ test/js/mljson-tests.js
@@ -1,138 +1,146 @@
-function jsonFromServer(json, success, error) {
-  asyncTest(json, function() {
-    $.ajax( { url: '/test/xq/isomorphic.xqy'
-    , data: 'json=' + json
-    , method: 'POST'
-    , success: success
-    , error: error
-    , complete: function() { start(); } }) }) }
+if(typeof mljson == "undefined" || !mljson) {
+    mljson = {};
+}
+
+$(document).ready(function() {
+    module("Bad JSON");
+    for (var i = 0; i < badJSON.length; i += 1) {
+        mljson.badFromServerTest(badJSON[i].json, badJSON[i].error);
+    }
+
+    module("Good JSON");
+    for (var i = 0; i < validJSON.length; i += 1) {
+        mljson.jsonFromServerTest(validJSON[i].json);
+    }
+
+    // Missing REST
+    // Missing Update Functions
+});
 
 
-function badFromServerTest(json, message) {
-  jsonFromServer( json,
-    function(d,t,j) { equals(d, message, 'NOK') },
-    function(j,t,e) { ok(true, 'OK') } ) }
+mljson.jsonFromServer = function(jsonString, success, error) {
+    asyncTest(jsonString, function() {
+        $.ajax({
+            url: '/test/xq/isomorphic.xqy',
+            data: 'json=' + jsonString,
+            method: 'POST',
+            success: success,
+            error: error,
+            complete: function() { start(); }
+        });
+    });
+};
 
-function jsonFromServerTest(json) {
-  jsonFromServer( json,
-    function(d,t,j) { 
-      equals(d, json, 'OK') },
-    function(j,t,e) { ok(false, 'NOK') } ) }
+mljson.badFromServerTest = function(json, message) {
+    mljson.jsonFromServer(json,
+        function(data, t, j) {
+            equals(data, message, 'NOK');
+        },
+        function(j, t, error) { equals(error, message, 'NOK'); }
+    );
+};
 
-function queryParser(json, path, success, error) {
-  asyncTest(json, function() {
-    $.ajax( { url: '/test/xq/parse.xqy'
-    , data: 'json=' + json + '&path=' + path
-    , method: 'POST'
-    , success: success
-    , error: error
-    , complete: function() { start(); } }) }) }
+mljson.jsonFromServerTest = function(json) {
+    mljson.jsonFromServer(JSON.stringify(json),
+        function(data, t, j) {
+            var isE = mljson.isEqual(JSON.parse(data), json);
+            if(!isE) {
+                console.log(JSON.stringify(json));
+                console.log(data);
+            }
+            ok(isE, 'OK');
+        },
+        function(j, t, e) { ok(false, 'NOK'); } 
+    );
+};
 
-function queryParserTest(json, path) {
-  queryParser( json, path,
-    function(d,t,j) { equals(d, path, 'OK') },
-    function(j,t,e) { ok(false, 'NOK') } ) }
+
 
 badJSON = [
     {
         "json": "[1 2]",
-        "error": "Logged Exception: Expecting end tag"
+        "error": "Unexpected token number: '[1 2]'. Expected either a comma or closing array"
     },
     {
         "json": "{\"foo\" \"bar\"}",
-        "error": "Logged Exception: Expecting end tag"
+        "error": "Unexpected token string: '{\"foo\" \"bar\"}'. Expected a colon"
     },
     {
         "json": "[1, 2",
-        "error": "Logged Exception: Expecting end tag"
+        "error": "Unexpected token : '1, 2'. Expected either a comma or closing array"
     },
     {
         "json": "[1.2.2]",
-        "error": "Logged Exception: Expecting end tag"
+        "error": "Unexpected token error: '[1.2.2]'. Expected either a comma or closing array"
     }
-]
+];
 
 validJSON = [
     {
-        "json": true,
-        "path": "/json"
+        "json": true
     },
     {
-        "json": false,
-        "path": "/json"
+        "json": false
     },
     {
-        "json": [],
-        "path": "/json"
+        "json": []
     },
     {
-        "json": null,
-        "path": "/json"
+        "json": null
     },
     {
-        "json": {},
-        "path": "/json"
+        "json": {}
     },
     {
         "json": [
             -1
-        ],
-        "path": "/json"
+        ]
     },
     {
         "json": [
             1.2
-        ],
-        "path": "/json"
+        ]
     },
     {
         "json": {
             "": "bar"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "_foo": "bar"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "f•o": "bar"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "foo": "bar\nbaz"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "foo": "\"bar\""
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "foo": "",
             "bar": ""
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "text": "ぐらまぁでちゅね♥おはようです！"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "text": "は 2010/11/4 at 5:50 AM に 6'45\"/km のペースで 8.42 km を走りました http://go.nike.com/9rlcovd"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": [
@@ -150,8 +158,7 @@ validJSON = [
                 6
             ],
             10
-        ],
-        "path": "/json"
+        ]
     },
     {
         "json": [
@@ -168,8 +175,7 @@ validJSON = [
                 ],
                 6
             ]
-        ],
-        "path": "/json"
+        ]
     },
     {
         "json": {
@@ -178,8 +184,7 @@ validJSON = [
                 "a": "b"
             },
             "bar": "aa"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -188,26 +193,22 @@ validJSON = [
                 "baz": 2,
                 "yaz": 3
             }
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "unquotedKey": []
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "whitespace": false
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
             "false": "false()"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": [
@@ -218,26 +219,22 @@ validJSON = [
             null,
             false,
             true
-        ],
-        "path": "/json"
+        ]
     },
     {
         "json": {
             "quoted": "This sentence is \"quoted\""
-        },
-        "path": "/json"
+        }
     },
     {
         "json": [
             "new\nline"
-        ],
-        "path": "/json"
+        ]
     },
     {
         "json": {
             "bad key": true
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -332,8 +329,7 @@ validJSON = [
                     ]
                 }
             }
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -389,8 +385,7 @@ validJSON = [
             "geo": null,
             "id": 29609921000,
             "truncated": false
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -459,12 +454,10 @@ validJSON = [
             "id": 29615391000,
             "truncated": false,
             "text": "Q quiere d aqui? No m puedo mover! RT @AlxRodz: @AnaHildaMedina Yo he ido! http://myloc.me/dOBlY"
-        },
-        "path": "/json"
+        }
     },
     {
-        "json": "おいおいどうして調子が悪いのｗｗｗｗ",
-        "path": "/json"
+        "json": "おいおいどうして調子が悪いのｗｗｗｗ"
     },
     {
         "json": {
@@ -486,8 +479,7 @@ validJSON = [
             "andPredicate": [],
             "descendant": {},
             "position": "1 to last()"
-        },
-        "path": "/json[book = (\"0596000405\")]"
+        }
     },
     {
         "json": {
@@ -510,8 +502,7 @@ validJSON = [
             "andPredicate": [],
             "descendant": {},
             "position": "1 to last()"
-        },
-        "path": "/json[id = (\"0596000405\", \"123456789\")]"
+        }
     },
     {
         "json": {
@@ -601,8 +592,7 @@ validJSON = [
                 "filtered": false,
                 "score": "logtfidf"
             }
-        },
-        "path": "cts:search(/json, cts:or-query((cts:element-value-query(fn:QName(\"\", \"greeting\"), \"Hello World\", (\"case-insensitive\",\"diacritic-sensitive\",\"punctuation-insensitive\",\"whitespace-insensitive\",\"stemmed\",\"wildcarded\",\"lang=en\"), 2), cts:element-word-query(fn:QName(\"\", \"para\"), \"Hello World\", (\"lang=en\"), 1), cts:element-range-query(fn:QName(\"\", \"para\"), \"=\", \"Hello World\", (\"collation=http://marklogic.com/collation/\"), 1), cts:element-pair-geospatial-query(fn:QName(\"\", \"location\"), fn:QName(\"\", \"latitude\"), fn:QName(\"\", \"longitude\"), (cts:point(\"53,12\"), cts:circle(\"@10 53,12\"), cts:box(\"[-5, -6, 3, 4]\"), cts:polygon(\"53,12 57,15 53,12\")), (\"coordinate-system=wgs84\"), 1), cts:collection-query(\"foo\"))), (\"unfiltered\", \"score-logtfidf\"), 1)"
+        }
     },
     {
         "json": {
@@ -612,8 +602,7 @@ validJSON = [
             "affiliation": "MarkLogic",
             "id": "43450202293176688",
             "bio": ""
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -623,8 +612,7 @@ validJSON = [
             "affiliation": "Company",
             "id": "93976451761512432",
             "bio": "Apart from living life with his wondrous family, Joe often finds himself with his hands on home row, typing with the intensity of a puma. \"To me, time at the keyboard with a good SDK is exactly like an artist at his canvas, only with a different palette,\" he says. He believes software is a fantastic creative medium because of its extreme malleability and availability. \"Anyone with a computer with an Internet connection has a fantastic opportunity to create something out of nothing that is original, useful, and beautiful.\" As of late, many of Joes creations have found new life on the MarkLogic platform. Working at the LDS Church, his teams have produced the new mormon.org, parts of lds.org, and are continuing to develop multiple new products for several missionary audiences. \"Its the golden age of software dev on the web,\" he declares. And who can doubt it, given the twinkle in his eye and not-so-subtle grin as he says it? Joe is excited to attend and share the MarkLogic love at this years conference and invites you to attend. "
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -638,8 +626,7 @@ validJSON = [
             "plenary": false,
             "startTime": "2011-04-29T11:15:00",
             "abstract": "How do you allow users to share and discover information in a classified environment?  ENSCO, Inc. has developed an innovative security application that allows users to ascertain the existence of documents without compromising access to the information.  See an example of a successful implementation of obtaining knowledge of the existence, but not necessarily the content, of  classified information.   \nWikiLeaks has illustrated that it is critically important to know what a user is doing.  Agencies must possess the ability to know who saw what, and when.  ENSCO has addressed this through the creation of an auditing service.   You will learn how this service tracks each users queries and provides a vehicle to monitor all access activities.\n"
-        },
-        "path": "/json"
+        }
     },
     {
         "json": {
@@ -658,8 +645,7 @@ validJSON = [
             "and": [],
             "innerKey": {},
             "position": "1 to last()"
-        },
-        "path": "/json[exists(book)][id = \"0596000405\" or other_id = \"0596000405\"]"
+        }
     },
     {
         "json": {
@@ -682,8 +668,7 @@ validJSON = [
             "and": [],
             "innerKey": {},
             "position": "1 to last()"
-        },
-        "path": "/json[book/id = \"0596000405\"][id = \"0596000405\" or other_id = \"0596000405\"]"
+        }
     },
     {
         "json": {
@@ -705,8 +690,7 @@ validJSON = [
             "and": [],
             "innerKey": {},
             "position": "1 to last()"
-        },
-        "path": "/json[exists(book/id)][id = \"0596000405\" or other_id = \"0596000405\"]"
+        }
     },
     {
         "json": {
@@ -740,8 +724,7 @@ validJSON = [
                 }
             ],
             "position": "1 to last()"
-        },
-        "path": "/json[book//id = (\"0596000405\", \"123456789\")][id = \"0596000405\" or other_id = \"0596000405\"][id = \"0596000405\" and other_id = \"0596000405\"]"
+        }
     },
     {
         "json": {
@@ -764,24 +747,86 @@ validJSON = [
                 }
             ],
             "position": "1 to 10"
-        },
-        "path": "(/json[book//id = (\"0596000405\", \"123456789\")][id = \"0596000405\" or other_id = \"0596000405\"])[1 to 10]"
+        }
     }
-]
+];
 
-$(document).ready(function(){
-  module("Bad JSON") ;
-    // Missing Invalid JSON
-    for (var i = badJSON.length - 1; i >= 0; i--) {
-      badFromServerTest(badJSON[i].json, badJSON[i].error) }
-  module("Good JSON") ;
-    for (var i = validJSON.length - 1; i >= 0; i--) {
-      jsonFromServerTest(JSON.stringify(validJSON[i].json)) }
-  module("Query Parser") ;
-    // Missing Invalid JSON
-    for (var i = validJSON.length - 1; i >= 0; i--) {
-      if (validJSON[i].path) { 
-        queryParserTest(JSON.stringify(validJSON[i].json), validJSON[i].path) } }
-  // Missing REST
-  // Missing Update Functions
-})
+/* Support functions */
+mljson.isEqual = function(a, b) {
+    var uniqueArray = function(a) {
+        if (!(a instanceof Array)) {
+            return;
+        }
+        a.sort();
+        for (var i = 1; i < a.length; i++) {
+            if (a[i - 1] == a[i]) {
+                a.splice(i, 1);
+            }
+        }
+    };
+
+    var generalType = function(o) {
+        var t = typeof(o);
+        if (t != 'object') {
+            return t;
+        }
+        if (o instanceof String) {
+            return 'string';
+        }
+        if (o instanceof Number) {
+            return 'number';
+        }
+        return t;
+    };
+
+    var keys = function(map) {
+        var list = [];
+
+        for (var prop in map) {
+            if (map.hasOwnProperty(prop)) {
+                list.push(prop);
+            }
+        }
+        return list;
+    }
+
+
+    if (a === b) {
+        return true;
+    }
+
+    if (generalType(a) != generalType(b)) {
+        return false;
+    }
+
+    if (a == b) {
+        return true;
+    }
+
+    if (typeof a != 'object') {
+        return false;
+    }
+
+    // null != {}
+    if (a instanceof Object != b instanceof Object) {
+        return false;
+    }
+
+    if (a instanceof Date || b instanceof Date) {
+        if (a instanceof Date != b instanceof Date ||
+            a.getTime() != b.getTime()) {
+            return false;
+        }
+    }
+
+    var allKeys = [].concat(keys(a), keys(b));
+    uniqueArray(allKeys);
+
+    for (var i = 0; i < allKeys.length; i++) {
+        var prop = allKeys[i];
+        if (!mljson.isEqual(a[prop], b[prop])) {
+            return false;
+        }
+    }
+    return true;
+};
--- test/xq/isomorphic.xqy
+++ test/xq/isomorphic.xqy
@@ -1,17 +1,11 @@
 xquery version "1.0-ml";
 
-import module namespace json = "http://marklogic.com/json" 
-  at "/lib/json.xqy" ;
+import module namespace json = "http://marklogic.com/json" at "/lib/json.xqy";
 
-declare variable $json := xdmp:get-request-field( "json" ) ;
-declare variable $transformed :=
-  fn:string( json:xmlToJSON( json:jsonToXML( $json )/* ) ) ;
-declare variable $valid := $json = $transformed ;
 
 try {
-$transformed,
-if( $valid ) then () else 
-  xdmp:log( ("Got: ", $transformed, "Expected: ", $json ) ) }
-catch ( $e ) {
-  fn:concat( "Logged Exception: ", $e//*:message ),
-  xdmp:log( ("Exception: ", $e, "Expected: ", $json ) ) }
+    json:xmlToJSON(json:jsonToXML(xdmp:get-request-field("json")))
+}
+catch ($e) {
+    xdmp:set-response-code(500, string($e//*:message))
+}
